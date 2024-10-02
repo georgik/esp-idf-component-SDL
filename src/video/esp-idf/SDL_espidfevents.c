5afa42d4cdd4566b114811fca306581dc43884ab
Juraj Michalek <juraj.michalek@gmail.com>
2024-10-02 18:37:34 +0200

    move ESP-IDF logic to the component

---
+++ src/video/esp-idf/SDL_espidfevents.c
@@ -0,0 +1,20 @@
+
+
+#include "SDL_internal.h"
+
+#ifdef SDL_VIDEO_DRIVER_ESP_IDF
+
+#include "events/SDL_events_c.h"
+#include "SDL_espidfvideo.h"
+#include "SDL_espidfevents.h"
+#include "SDL_espidftouch.h"
+
+void ESPIDF_PumpEvents(SDL_VideoDevice *_this)
+{
+    // Placeholder for actual event polling implementation
+    // Depending on the platform, poll for touchscreen or other events here.
+
+    ESPIDF_PumpTouchEvent();
+}
+
+#endif /* SDL_VIDEO_DRIVER_ESP_IDF */
